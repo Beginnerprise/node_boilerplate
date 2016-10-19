bbeaaf05fd2085fdfc73488f2b25689bc24163f7
Benjamin Hudgens <benjamin@benjamindavid.com>
2016-10-18 23:28:13 -0500

    Less wordy

--- config/config.js
+++ config/config.js
@@ -5,15 +5,9 @@
  ********************************************************************/
 
 /*eslint no-process-env: "off"*/
-var config = {
+module.exports = {
   logging: typeof process.env.ENABLE_LOGGING !== "undefined",
   debug: typeof process.env.ENABLE_DEBUG !== "undefined",
   verbose: typeof process.env.ENABLE_VERBOSE !== "undefined",
   serverPort: process.env.PORT || 3000
 };
-
-/********************************************************************
- * Export
- ********************************************************************/
-
-module.exports = config;
